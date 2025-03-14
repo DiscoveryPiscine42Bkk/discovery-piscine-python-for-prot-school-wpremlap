86c3bd256adac2dd46ea82c73b7c60cf4eae0280
wpremlap <40315@prot.ac.th>
2025-03-14 14:22:33 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+
+new_array = [x + 2 for x in original_array]
+
+
+print("Original array:", original_array)
+print("New array with 2 added to each value:", new_array)
